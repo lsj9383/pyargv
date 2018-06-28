aa55c368cd9f2cef17e82506f0a8777241389505
asir <asirlu@foxmail.com>
2018-06-28 11:22:39 +0800

    添加关键词参数

--- pyargv.py
+++ pyargv.py
@@ -24,26 +24,50 @@ class Boolean(BaseArgv):
     def __init__(self, key, default=False):
         super().__init__(key, default)
 
-def __kwsload__(kws, norm_argvlist, bool_argvlist):
-    normargs = [argv for argv in sys.argv[1:] if not argv.startswith("--")]
-    boolargs = [argv for argv in sys.argv[1:] if argv.startswith("--")]
+class KeyValue(BaseArgv):
+    def __init__(self, key, nick=None, default=None):
+        super().__init__(key, default)
+        self.__nick__ = nick
+
+    @property
+    def nick(self):
+        return self.__nick__
+
+def __kwsload__(kws, norm_argvlist, bool_argvlist, kv_argvlist):
+    argvline = " ".join(sys.argv[1:])
+    for argv in kv_argvlist:
+        argvline = argvline.replace(argv.nick+" ", argv.key+":")
+    cmdargv = argvline.split(" ")
+    normargs = [argv for argv in cmdargv if not argv.startswith("--")]
+    boolargs = [argv for argv in cmdargv if argv.startswith("--")]
+    mapargs = [argv for argv in cmdargv if ":" in argv]
     for idx, argv in enumerate(norm_argvlist):
         kws[argv.key] = normargs[idx] if idx < len(normargs) else argv.default
 
     for idx, argv in enumerate(bool_argvlist):
         kws[argv.key] = argv.default
+
+    for argv in kv_argvlist:
+        kws[argv.key] = argv.default
+
     for argv in boolargs:
         kws[argv[2:]] = True if argv[2:] in argv else kws[argv[2:]]
-
+    for argv in mapargs:
+        parts = argv.split(":")
+        key = parts[0]
+        val = ":".join(parts[1:])
+        if key in kws:
+            kws[key] = val
     
 
-def argvload(argvlist=()):
+def argvload(argvlist=(), help=False):
     bool_argvlist = [argv for argv in argvlist if isinstance(argv, Boolean)]
+    kv_argvlist = [argv for argv in argvlist if isinstance(argv, KeyValue)]
     norm_argvlist = [argv for argv in argvlist if isinstance(argv, Argv)]
     def wrapper(f):
         @wraps(f)
         def inner(*args, **kws):
-            __kwsload__(kws, norm_argvlist, bool_argvlist)
+            __kwsload__(kws, norm_argvlist, bool_argvlist, kv_argvlist)
             return f(*args, **kws)
         return inner
     return wrapper
--- test.py
+++ test.py
@@ -3,14 +3,16 @@ import pyargv
 
 @pyargv.argvload((
     pyargv.Argv("origin"),
-    pyargv.Argv("to",),
+    pyargv.Argv("to"),
     pyargv.Argv("alpha", 13),
+    pyargv.KeyValue("beta", "-b", 456),
     pyargv.Boolean("debug"),
     ))
-def main(origin, to, alpha, debug):
+def main(origin, to, alpha, beta, debug):
     print("origin:", origin)
     print("to:", to)
     print("alpha:", alpha)
+    print("beta:", beta)
     print("debug:", debug)
 
 if __name__ == "__main__":
