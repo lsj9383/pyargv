f677dd561e80ba7e2d6942f7a09639087b67096d
asir <asirlu@foxmail.com>
2018-06-28 09:36:47 +0800

    添加位置参数 默认参数 布尔参数

--- pyargv.py
+++ pyargv.py
@@ -1,14 +1,49 @@
 import os
+import sys
 from functools import wraps
 from flask import Flask
 
-def __around__(f):
-    pass
+class BaseArgv:
+    def __init__(self, key, default):
+        self.__key__ = key
+        self.__default__ = default
 
-def argvlist():
+    @property
+    def key(self):
+        return self.__key__
+
+    @property
+    def default(self):
+        return self.__default__
+
+class Argv(BaseArgv):
+    def __init__(self, key, default=None):
+        super().__init__(key, default)
+        
+class Boolean(BaseArgv):
+    def __init__(self, key, default=False):
+        super().__init__(key, default)
+
+def __kwsload__(kws, norm_argvlist, bool_argvlist):
+    normargs = [argv for argv in sys.argv[1:] if not argv.startswith("--")]
+    boolargs = [argv for argv in sys.argv[1:] if argv.startswith("--")]
+    for idx, argv in enumerate(norm_argvlist):
+        kws[argv.key] = normargs[idx] if idx < len(normargs) else argv.default
+
+    for idx, argv in enumerate(bool_argvlist):
+        kws[argv.key] = argv.default
+    for argv in boolargs:
+        kws[argv[2:]] = True if argv[2:] in argv else kws[argv[2:]]
+
+    
+
+def argvload(argvlist=()):
+    bool_argvlist = [argv for argv in argvlist if isinstance(argv, Boolean)]
+    norm_argvlist = [argv for argv in argvlist if isinstance(argv, Argv)]
     def wrapper(f):
         @wraps(f)
         def inner(*args, **kws):
-             return __around__(f)
+            __kwsload__(kws, norm_argvlist, bool_argvlist)
+            return f(*args, **kws)
         return inner
-    return wapper
+    return wrapper
--- test.py
+++ test.py
@@ -1,9 +1,17 @@
 import pyargv
 
 
-@pyargv.argvlist()
-def main():
-    print("hello world")
+@pyargv.argvload((
+    pyargv.Argv("origin"),
+    pyargv.Argv("to",),
+    pyargv.Argv("alpha", 13),
+    pyargv.Boolean("debug"),
+    ))
+def main(origin, to, alpha, debug):
+    print("origin:", origin)
+    print("to:", to)
+    print("alpha:", alpha)
+    print("debug:", debug)
 
 if __name__ == "__main__":
     main()
